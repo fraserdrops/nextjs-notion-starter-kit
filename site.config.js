c02ac46882c443156500181469bb79c953f42da3
Fraser McIntosh <frasermcintosh1@gmail.com>
2021-11-03 21:18:12 +1300

    Update site config

--- site.config.js
+++ site.config.js
@@ -1,25 +1,24 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'ed4369566a734ec8b3d49c83134d4eed',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Fraser',
+  domain: 'fray.dev',
+  author: 'Fraser',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: '',
+  socialImageTitle: 'Fraser',
+  socialImageSubtitle: '',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'fraser_drops',
+  github: 'fraserdrops',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
